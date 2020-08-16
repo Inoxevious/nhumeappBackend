b6322aa7fd7264c234b7f1ec7f5f133e4602df51
Inoxevious <mpasiinnocent@gmail.com>
2020-08-15 20:35:11 -0400

    images url

--- grabitapp/urls.py
+++ grabitapp/urls.py
@@ -33,3 +33,4 @@ urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
     path('admin/', admin.site.urls),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
